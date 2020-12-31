d2c5ff67236dc1aaa2891c990c41d444d2f21df8
Andyloris <63070052+Andyloris@users.noreply.github.com>
2020-12-31 23:15:50 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,10 @@ function setup() {
 function draw() {
   colorMode(RGB);
   background(0, 0, 0, 25);
-  
+  var now = new Date();
+  if((now.getTime() - (new Date("Jan 1, 2021 00:00").getTime()) == 0)) {
+    document.getElementById('fest').innerHtml = 'Happy New Year';
+  }
   if (random(1) < 0.25) {
     fireworks.push(new Firework());
   }
